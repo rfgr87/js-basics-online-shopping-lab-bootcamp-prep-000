8518b6cd4dc2f1fe63bc1096b75a8eb585521497
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 20:43:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function addToCart(item) {
   var array = []
   var price = Math.floor(Math.random() * 99) + 1
   var cart = new Object();
-  cart.itemName{itemName: item, itemPrice: price}
+  cart.itemName = `${item}`;
+  cart.itemPrice = `${price}`
   array.push(cart)
   return `<${item}> has been added to your cart.`
 }
