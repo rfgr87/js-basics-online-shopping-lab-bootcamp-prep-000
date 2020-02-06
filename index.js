108eea9d3548f779cf91d12593642aec7e2ce837
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 22:36:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,11 +79,13 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
+  var string;
   if(!cardNumber){
-    return "Sorry, we don't have a credit card on file for you."
+    string = "Sorry, we don't have a credit card on file for you."
   }
   else {
-    cart = [];
-    return "Your total cost is $" + total() +", which will be charged to the card " + cardNumber + ".";
+    string = "Your total cost is $" + total() +", which will be charged to the card " + cardNumber + ".";
   }
+  cart = [];
+  return string
 }
