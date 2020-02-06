cf4a2bb5f2191130d5e097f9a1a1610730ed6d55
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 21:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,10 +65,13 @@ function total() {
 function removeFromCart(item) {
   for(let i = 0; i<cart.length; i++){
     if(cart[i].itemName === item){
-      cart[i].splice(i,1)
+      cart[i].splice(i,1);
+      return cart;
     }
+    else{
+      return "That item is not in your cart."
   }
- 
+} 
 }
 
 function placeOrder(cardNumber) {
