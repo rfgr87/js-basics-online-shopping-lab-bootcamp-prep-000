f8d838111515f8219f18364628ea2f2f659cdf7d
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 22:31:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if(cardNumber === false){
+  if(!cardNumber){
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
